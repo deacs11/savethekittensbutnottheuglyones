b5304ea26fcedb1e02006f9d937d0a1e1c675b0c
Sergiu <s.deaconu97@gmail.com>
2023-09-14 17:28:57 +0200

    Initial Commit

---
+++ SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensExceptTheHairlessOnesApp.swift
+//  SaveTheKittensExceptTheHairlessOnes
+//
+//  Created by Sergiu Deaconu on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensExceptTheHairlessOnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
